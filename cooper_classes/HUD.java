f2291e01ed9d02166ecf5f12cabe365b3b393ea1
ryancooper7 <ryan.cooper7@gmail.com>
2018-03-20 22:01:37 -0600

    Added some basic outlines for classes

---
+++ cooper_classes/HUD.java
@@ -0,0 +1,15 @@
+
+public class HUD implements Drawable{
+
+	private int score;
+	
+	@Override
+	public void Draw() {
+		//needs implementation
+	}
+	
+	public void SetScoreDisplay(int newScore) {
+		score = newScore;
+	}
+	
+}
---
+++ cooper_classes/ScreenDisplay.java
@@ -0,0 +1,29 @@
+
+public class ScreenDisplay implements Drawable{
+
+	private int elementCount;
+	
+	public ScreenDisplay() {
+		elementCount = 0;
+	}
+	
+	@Override
+	public void Draw() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void AddElement(String name, String text, float xpos, float ypos, float scale) {
+		
+	}
+	
+	public String GetElement(String name) {
+		
+	}
+	
+	public void SetElement(String name, String text) {
+		
+	}
+	
+
+}
