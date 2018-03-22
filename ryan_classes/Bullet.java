310c3ab4fd5e6f9d3fbe129958ef6e6d791d3170
chda6313 <chda6313@colorado.edu>
2018-03-21 23:33:05 -0600

    added progress diagram, updates some classes

--- ryan_classes/Bullet.java
+++ ryan_classes/Bullet.java
@@ -1,19 +1,30 @@
 class Bullet extends DynamicObject
 {
-  public Bullet()
+  public float velocity;
+  public Bullet(float v)
   {
     super();
     super.setSprite("bullet");
     super.setDeathSprite("bulletDeath");
+    this.velocity = v
   }
   
-  public Bullet(float x, float y, float s, float th, float sp, float dat)
+  public Bullet(float x, float y, float s, float th, float sp, float dat, float v)
   {
     super(x, y, s, th, "bullet", sp, dat, "bulletDeath");
+    this.velocity = v;
   }
   
   public void DoMove()
   {
     //move one speed's distance foward
+  }
+  public boolean checkPlayerDeath(Enemy E){
+    //check this against enemy position, if overlap, kill the player.
+    if (this.ypos == E.ypos) && (this.xpos == E.xpos){//should depend on the size of the sprites
+      return true;
+    }
+    return false;
+    //probably change the function name, as player shoots bullets at enemies
   }
 }
--- ryan_classes/Wall.java
+++ ryan_classes/Wall.java
@@ -10,4 +10,11 @@ class Wall extends GameObject
   {
     super(x, y, scale, rotation, "wall");
   }
+  public boolean checkCollision(GameObject checkAgainst)
+  {
+    if (this.ypos == checkAgainst.ypos) && (this.xpos == checkAgainst.xpos){//should depend on the size of the sprite of checkAgainst
+      return true;
+    }
+    return false;
+  }
 }
