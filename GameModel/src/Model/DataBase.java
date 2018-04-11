290ee905e3374e98afa61d7cfc83a8faf93f48e4
davidnyberg <dnyberg95@gmail.com>
2018-04-11 11:48:14 -0600

    Add files via upload

--- GameModel/src/Model/DataBase.java
+++ GameModel/src/Model/DataBase.java
@@ -9,23 +9,32 @@ import java.sql.DriverManager;
 
 class DataBase {
 
+    private String sqlInsert = "";
+    private String sqlReturn = "";
+
     DataBase(){
-        String url = "jdbc:mysql://localhost:3306/test?useSSL=false";
-        String user = "root";
-        String pass = "root";
+        final String URL = "jdbc:mysql://localhost:3306/highscores?useSSL=false";
+        final String USER = "root";
+        final String PASS = "root";
         //Connection con;
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            DriverManager.getConnection(url, user, pass);
+            System.out.println("Connecting to highscores database...");
+            DriverManager.getConnection(URL, USER, PASS);
 
         } catch (Exception e) {
-            System.out.println("Caught error: " + e);
+            System.out.println("error: " + e);
         }
+        System.out.println("Success!");
+
     }
 
     public void getScore() {
         //gets the score from the controller after a game is finished, can use SQL to put into the DB
+    }
+    public void insertScore(){
+        //insert the score into the mysql database
     }
 
     public int returnScore() {
