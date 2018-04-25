a651752e3be45d418c751ecfa24e7d9099b26432
davidnyberg <dnyberg95@gmail.com>
2018-04-25 15:08:16 -0600

    Add files via upload

--- GameModel/src/Model/Wall.java
+++ GameModel/src/Model/Wall.java
@@ -1,14 +1,30 @@
 package Model;
 
+import javax.imageio.ImageIO;
+import javax.sound.midi.SysexMessage;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Wall extends GameObject {
-    
+
     public Wall(float xpos, float ypos, float scale, float rotation, BufferedImage sprite)
     {
         super(xpos, ypos, scale, rotation, sprite);
     }
 
+    @Override
+    public void render(Graphics g) {
+        Image img = null;
+        try{
+            img = ImageIO.read(new File("/Users/davidnyberg/Desktop/turtle.png"));
+
+        } catch (IOException e) {
+            System.out.println(e);
+            }
+
+            g.drawImage(img,(int)getXpos(), (int)getYpos(),40,40,null);
+    }
 }
